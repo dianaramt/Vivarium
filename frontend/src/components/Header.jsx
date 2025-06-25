7d8398fdc34c25ae02aeb3f8a580f2ef3120f610
Diana <dianaramos19@gmail.com>
2025-06-25 20:55:15 +0200

    probando estilos no menú

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
       <div className="max-w-6xl mx-auto px-4 py-4 flex items-center justify-between">
         <h1 className="text-xl font-bold">🌿 Vivarium</h1>
         <nav className="space-x-4 hidden sm:block">
-          <a href="#" className="hover:underline">Inicio</a>
-          <a href="#" className="hover:underline">Especies</a>
-          <a href="#" className="hover:underline">Sobre nós</a>
+          <a href="#" className="bg-yellow-600 px-4 py-2 rounded text-black hover:bg-white">Inicio</a>
+          <a href="#" className="bg-yellow-600 px-4 py-2 rounded text-black hover:bg-white">Datos curiosos</a>
+          <a href="#" className="bg-yellow-600 px-4 py-2 rounded text-black hover:bg-white">Reinos</a>
         </nav>
         <button className="sm:hidden p-2">☰</button>
       </div>
